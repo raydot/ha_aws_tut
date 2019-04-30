64bdfc46576bafa9ff4a355feb1b827060a39e0d
Dave Kanter <moatgoat@hotmail.com>
2019-04-29 17:51:01 -0700

    First commit.

---
+++ package.json
@@ -0,0 +1,10 @@
+{
+  "name": "Nodejs-DB",
+  "description": "Node.js example application",
+  "version": "0.0.1",
+  "dependencies": {
+    "express": "*",
+    "ejs": "*",
+    "mysql": "*"
+  }
+}
---
+++ server.js
@@ -0,0 +1,66 @@
+//server.js
+
+/* *************************
+IMPORTANT!  Important!
+
+With AWS OpsWorks Stacks, a Node.js application's main code file must be named server.js and be located in the application's root folder.
+**************************** */
+
+var express = require('express');
+var mysql = require('mysql');
+var dbconfig = require('opsworks'); //[1] include db connection data
+var app = express();
+var outputString = "";
+
+app.engine('html', require('ejs').renderFile);
+
+//[2] Get database connection data
+app.locals.hostname = dbconfig.db['host']
+app.locals.username = dbconfig.db['username']
+app.locals.password = dbconfig.db['password']
+app.locals.port = dbconfig.db['port']
+app.locals.connectionerror = 'successful'
+app.locals.databases = ''
+
+//[3] Connect to the Amazon RDS instance
+var connection = mysql.createConnection({
+	host: dbconfig.db['host'],
+	user: dbconfig.db['username'],
+	password: dbconfig.db['password'],
+	port: dbconfig.db['port'],
+	database: dbconfig.db['database']
+});
+
+connection.connect(function(err) {
+	if (err) {
+		app.locals.connectionerror = err.stack;
+		return;
+	}
+});
+
+// [4] Query the database
+connection.query('SHOW DATABASES', function (err, results) {
+	if(err) {
+		app.locals.databases = err.stack;
+	}
+
+	if (results) {
+		for (var i in results) {
+			outputString = outputString + results[i].Database + ', '
+		}
+		app.locals.databases = outputString.slice(0, outputString.length-2)
+	}
+});
+
+connection.end();
+
+app.get('/', function(req, res) {
+	res.render('./index.html');
+});
+
+app.use(express.static('public'));
+
+//[5] Listen for incoming requests
+app.listen(process.env.PORT);
+
+
